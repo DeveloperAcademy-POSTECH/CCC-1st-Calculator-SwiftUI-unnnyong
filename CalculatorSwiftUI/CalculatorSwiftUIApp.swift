f601531916b35af1c6bdb6dec34d178f2fac2716
unnnyong <unnnyong@gmail.com>
2022-06-25 17:19:26 +0900

    create Xcode project

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Eunyeong Kim on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
